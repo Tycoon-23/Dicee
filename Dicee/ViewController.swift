e241679132cb624cc14091fb1d59c2645f628c9e
Aditya Vyavahare <vvaditya23@icloud.com>
2022-07-28 22:18:13 +0530

    Completed the UI.

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftDiceView: UIImageView!
+    @IBOutlet weak var rightDiceView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButton(_ sender: UIButton) {
+    }
+    
 }
 
