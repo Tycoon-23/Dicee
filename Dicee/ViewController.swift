0259a78d243e997746778a9124f172338689227a
Aditya Vyavahare <vvaditya23@icloud.com>
2022-07-29 13:25:47 +0530

    applied who.what = value

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -12,13 +12,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var leftDiceView: UIImageView!
     @IBOutlet weak var rightDiceView: UIImageView!
     
+    //let diceArray = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        leftDiceView.alpha = 0.5
+        rightDiceView.image = imageLiteral
     }
 
     @IBAction func rollButton(_ sender: UIButton) {
+        
+        
+        
     }
     
 }
